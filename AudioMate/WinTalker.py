9f9d1ecf5b7fa6bd94b7dd24d497a6b07ae0db57
Rich Eaton <richeaton@verizon.net>
2021-08-13 10:37:35 -0400

    First Pass

---
+++ AudioMate/WinTalker.py
@@ -0,0 +1,36 @@
+import pyttsx3
+import PySimpleGUI as sg 
+
+text = sg.popup_get_file('Please enter a file name')
+# sg.popup('Results', 'The value returned from popup_get_file', text)
+
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+infile = 'PyAudioReader\TestFile.txt'
+f = open(text, 'r')
+theText = f.read()
+f.close()
+engine.say(theText)
+engine.runAndWait()
+
+"""
+sg.theme('DarkAmber')   # Add a touch of color
+
+# All the stuff inside your window.
+layout = [[sg.Text('Some text on Row 1')],
+          [sg.Text('Enter something on Row 2'), sg.InputText()],
+          [sg.Button('Ok'), sg.Button('Cancel')]]
+
+# Create the Window
+
+window = sg.Window('Audio Reader v.001', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+        break
+    print('You entered ', values[0])
+
+window.close()
+"""
